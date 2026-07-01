a6edbb278d61f4ddf590dcb2b977d59fce49c2c6
Aldrian <sabilealdrian51@gmail.com>
2026-07-01 22:48:01 +0800

    rehaul of design

--- scripts.js
+++ scripts.js
@@ -11,6 +11,11 @@ async function run(city_name) {
         const response = await fetch (`https://geocoding-api.open-meteo.com/v1/search?name=${city_name}&count=10&language=en&format=json`)
         const data = await response.json()
         const { results } = data
+        if (!results || results.length === 0){
+            document.querySelector(".display-weather").innerHTML = `
+            <h2>no data found... </h2>`
+            return
+        }
         const {name, latitude, longitude} = results[0]
         const weather_response = await fetch (`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`)
         const weather_data = await weather_response.json()
@@ -26,5 +31,5 @@ function display(name, temperature, weathercode) {
     document.querySelector(".display-weather").innerHTML = `
     <h2>${name}</h2>
     <p>Temperature: ${temperature}°C</p>
-    <P>Weather Code: ${weathercode}`
+    <p>Weather Code: ${weathercode}</p>`
 }
